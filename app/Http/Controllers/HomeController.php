fe584a15f21075a225706da1f8035eecf7538e9f
Gilberto Russi <gilesrussi@gmail.com>
2016-06-14 09:20:44 -0300

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,9 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth', array(
+            'except' => array('index')
+        ));
     }
 
     /**
